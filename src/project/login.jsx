ab42ca4d587a3c10d7208359e9e111626bfffac8
Bluebird5757 <136193863+Bluebird5757@users.noreply.github.com>
2024-04-09 17:06:51 +0530

    changed

--- src/project/login.jsx
+++ src/project/login.jsx
@@ -4,6 +4,7 @@ import { Routes,Route } from 'react-router-dom';
 import { useNavigate, useParams } from 'react-router-dom'; 
 import Growerdash from './growerdash';
 export default function Login() {
+    console.log("fsfdsdf");
     var navigate=useNavigate();
     const [userobj, setUserObj] = useState({
         email: "",
